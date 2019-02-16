f133952881af75d41fbcd4055b1f449e501709fb
Igor Brandão <igorabrandao@gmail.com>
2019-02-16 11:55:10 -0300

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './layout/admin/admin.component';
 import { AuthComponent } from './layout/auth/auth.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SharedModule} from './shared/shared.module';
-import {MenuItems} from './shared/menu-items/menu-items';
-import {BreadcrumbsComponent} from './layout/admin/breadcrumbs/breadcrumbs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { MenuItems } from './shared/menu-items/menu-items';
+import { BreadcrumbsComponent } from './layout/admin/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
