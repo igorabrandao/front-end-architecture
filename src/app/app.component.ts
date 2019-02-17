6a77264d4b1f32d888b81b9c73b929078c3e5082
Igor Brandão <igorabrandao@gmail.com>
2019-02-17 14:54:15 -0300

    Change project basic settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {NavigationEnd, Router} from '@angular/router';
 })
 
 export class AppComponent implements OnInit {
-  title = 'Mega Able 5+';
+  title = 'Front-end architecture 5+';
 
   constructor(private router: Router) { }
 
